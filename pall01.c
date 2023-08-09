3802c8ee606bf8a1830c10f0ebe1c2c0fbb977fa
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-09 22:41:15 +0530

    Update pall01.c

--- pall01.c
+++ pall01.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int isPalindrome(int num) {
+    int originalNum = num;
+    int reverse = 0;
+    
+    while (num > 0) {
+        int digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num /= 10;
+    }
+    
+    return originalNum == reverse;
+}
+
+int main() {
+    int T; // Number of test cases
+    scanf("%d", &T);
+
+    while (T--) {
+        int N; // Input number
+        scanf("%d", &N);
+
+        if (isPalindrome(N)) {
+            printf("wins\n");
+        } else {
+            printf("loses\n");
+        }
+    }
+
+    return 0;
+}
