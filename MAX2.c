7c32e691e1f5080cb4d66bd9fc34a199c0451a9e
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-11 23:21:42 +0530

    Update MAX2.c

--- MAX2.c
+++ MAX2.c
@@ -1,1 +1,23 @@
 
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    
+    char binary[N + 1];
+    scanf("%s", binary);
+
+    int maxPower = 0;
+    for (int i = N - 1; i >= 0; i--) {
+        if (binary[i] == '1') {
+            printf("%d\n", maxPower);
+            break;
+        }
+        maxPower++;
+    }
+
+    return 0;
+}
