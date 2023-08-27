16107d03d97eca0dbf569f1a8c0d2a94680f6eb2
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-27 23:42:37 +0530

    Update EZSPEAK.c

--- EZSPEAK.c
+++ EZSPEAK.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+  int t, n;
+  char s[101];
+
+  scanf("%d", &t);
+  for (int i = 0; i < t; i++) {
+    scanf("%d %s", &n, s);
+
+    int count = 0;
+    int vowel = 0;
+    for (int j = 0; j < n; j++) {
+      if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u') {
+        vowel = 1;
+        count = 0;
+      } else {
+        count++;
+        if (count == 4) {
+          printf("NO\n");
+          break;
+        }
+      }
+    }
+
+    if (count < 4 || !vowel) {
+      printf("YES\n");
+    }
+  }
+
+  return 0;
+}
