ed6da7417846abd4f5b4efaca6114346075298c4
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-28 23:54:45 +0530

    Update WAV2.c

--- WAV2.c
+++ WAV2.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main() {
+  int n, q;
+  scanf("%d %d", &n, &q);
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
+  for (int i = 0; i < q; i++) {
+    int x;
+    scanf("%d", &x);
+    int product = 1;
+    for (int j = 0; j < n; j++) {
+      if (x != a[j]) {
+        product *= x - a[j];
+      }
+    }
+    if (product > 0) {
+      printf("POSITIVE\n");
+    } else if (product < 0) {
+      printf("NEGATIVE\n");
+    } else {
+      printf("0\n");
+    }
+  }
+  return 0;
+}
 
